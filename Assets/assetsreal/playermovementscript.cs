d8a0600974046e6924dbf811deefc531334f2bba
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2022-09-19 11:20:26 +0100

    feat: initial commit

---
+++ Assets/assetsreal/playermovementscript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovementscript : MonoBehaviour
+{   
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = 20.0f;
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.isGrounded)
+       {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform. right * x +transform.forward *z;
+        controller.Move(move * speed* Time.deltaTime);
+
+       }
+        //This is for gravity effects
+        moveDirection.y -= gravity * Time.deltaTime;
+        //For moving the controller
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
---
+++ Assets/character bvhvjhbv/destroyPrincess.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyPrincess : MonoBehaviour
+{
+    void Start()
+ {
+     StartCoroutine(SelfDestruct());
+ }
+
+
+    IEnumerator SelfDestruct()
+ {
+     yield return new WaitForSeconds(65f);
+     Destroy(gameObject);
+ }
+}
