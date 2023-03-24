eb7fa883ed13cf1117dbee13e3ff707427c71055
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2023-03-24 09:15:59 +0100

    add: LoadNextScene.cs

---
+++ Assets/Scripts/LoadNextScene.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class LoadNextScene : MonoBehaviour
+{
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+                EditorApplication.ExitPlaymode();
+        #else
+                Application.Quit();
+        #endif
+    }
+}
